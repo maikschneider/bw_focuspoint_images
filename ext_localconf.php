5e68d858285f9297d02b8235f78bfb012d7f96a3
Maik Schneider <m.schneider@blueways.de>
2020-05-13 15:18:50 +0200

    feat: content element preview renderer

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 <?php
+
+use Blueways\BwFocuspointImages\Hooks\BwFocuspointSvgPreviewRenderer;
+
 defined('TYPO3_MODE') || die('Access denied');
 
 $_EXTKEY = 'bw_focuspoint_images';
@@ -21,3 +24,6 @@ $iconRegistry->registerIcon(
     \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
     ['source' => 'EXT:bw_focuspoint_images/Resources/Public/Icons/ContentElements/FocuspointSVG.svg']
 );
+
+// Register for hook to show preview of tt_content element of CType="textmedia" in page module
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['bw_focuspoint_images_svg'] = BwFocuspointSvgPreviewRenderer::class;
