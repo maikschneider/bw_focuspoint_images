754cd4d0f12f011557ff21bcf79a03766cf5f983
Maik Schneider <m.schneider@blueways.de>
2020-05-14 13:13:03 +0200

    chore: cleanup

--- Classes/DataProcessing/FocuspointProcessor.php
+++ Classes/DataProcessing/FocuspointProcessor.php
@@ -32,18 +32,17 @@ class FocuspointProcessor extends FilesProcessor
         // the TCA is configured to use max. 1 image, however the file collector returns an array
         foreach ($processedData['image'] as $key => $file) {
 
-            $points = $file->getProperty('focus_points') ?: "[]";
-            $points = json_decode($points);
+            $points = $file->getProperty('focus_points') ?: '[]';
+            $points = json_decode($points, false);
 
             foreach ($points as $point) {
-                $point->x = $point->x * 100;
-                $point->y = $point->y * 100;
-                $point->height = $point->height * 100;
-                $point->width = $point->width * 100;
+                $point->x *= 100;
+                $point->y *= 100;
+                $point->height *= 100;
+                $point->width *= 100;
                 // calculate center of each point for text positioning
                 $point->textX = $point->x + ($point->width / 2);
                 $point->textY = $point->y + ($point->height / 2);
-                $point->textXinvert = 100 - $point->textX;
             }
 
             $processedData['image'] = $file;
