ca4131b5599c5825cea2980a09387893773b1624
Maik Schneider <m.schneider@blueways.de>
2021-09-08 22:17:48 +0200

    feat: remove old link browser php code

--- Classes/Form/Wizard/FocusPointWizard.php
+++ Classes/Form/Wizard/FocusPointWizard.php
@@ -38,6 +38,12 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 class FocusPointWizard
 {
 
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @return \TYPO3\CMS\Core\Http\JsonResponse
+     * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
+     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
+     */
     public function getLinkWizardUrlAction(ServerRequestInterface $request): JsonResponse
     {
         $queryParams = $request->getQueryParams();
@@ -92,23 +98,15 @@ class FocusPointWizard
 
     /**
      * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface $response
-     * @return \Psr\Http\Message\ResponseInterface
-     * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
-     * @throws \TYPO3\CMS\Fluid\View\Exception\InvalidTemplateResourceException
+     * @return \TYPO3\CMS\Core\Http\Response
      */
-    public function getWizardAction(ServerRequestInterface $request, ResponseInterface $response = null)
+    public function getWizardAction(ServerRequestInterface $request): Response
     {
-        if (null === $response) {
-            $response = new Response();
-        }
+        $response = new Response();
 
         if (!$this->isSignatureValid($request)) {
             return $response->withStatus(403);
         }
-
-        // @TODO: do not read TypoScript, use PageTS
-        $typoScript = HelperUtility::getTypoScript();
 
         $templateView = GeneralUtility::makeInstance(StandaloneView::class);
         $templateView->setLayoutRootPaths(['EXT:bw_focuspoint_images/Resources/Private/Layouts']);
@@ -129,13 +127,9 @@ class FocusPointWizard
             }
         }
 
-        $linkBrowsers = $this->linkBrowser($queryParams['pid'], $typoScript);
-        $tree = $this->treeAction($queryParams['pid']);
         $viewData = [
             'image' => $image,
-            'focusPoints' => $queryParams['focusPoints'],
-            'linkBrowsers' => $linkBrowsers,
-            'tree' => $tree
+            'focusPoints' => $queryParams['focusPoints']
         ];
         $templateView->assignMultiple($viewData);
         $content = $templateView->render();
@@ -154,95 +148,5 @@ class FocusPointWizard
     {
         $token = GeneralUtility::hmac($request->getQueryParams()['arguments'], 'ajax_wizard_focuspoint');
         return $token === $request->getQueryParams()['signature'];
-    }
-
-    protected function linkBrowser(int $pid, array $typoscript)
-    {
-        $linkFields = array_filter($typoscript['settings']['fields'], function ($field) {
-            return isset($field['type']) && $field['type'] === 'link';
-        });
-
-        // abort if no link browser is required
-        if (!count($linkFields)) {
-            return [];
-        }
-
-        // guess the entry point for linkhandler configuration and filter for ones with configuration (table name)
-        $pageTsConfig = HelperUtility::getPagesTSconfig($pid);
-        $linkBrowsers = array_filter($pageTsConfig['TCEMAIN']['linkHandler'], function ($linkBrowser) {
-            return
-                isset($linkBrowser['handler'], $linkBrowser['configuration']) && substr(strrchr($linkBrowser['handler'],
-                    "\\"), 1) === 'RecordLinkHandler';
-        });
-
-        // set storage pids
-        foreach ($linkBrowsers as &$linkBrowser) {
-            if (isset($linkBrowser['configuration']['storagePid'])) {
-                continue;
-            }
-
-            if (!isset($linkBrowser['configuration']['storagePid']) && isset($linkBrowser['configuration']['onlyPids'])) {
-                $linkBrowser['configuration']['storagePid'] = $linkBrowser['configuration']['onlyPids'];
-                continue;
-            }
-
-            $linkBrowser['configuration']['storagePid'] = $pid;
-            $linkBrowser['configuration']['levels'] = 9;
-        }
-
-        return $linkBrowsers;
-    }
-
-    protected function treeAction(int $startingPid)
-    {
-        // Get starting point and page record of it
-        $rootLine = BackendUtility::BEgetRootLine($startingPid);
-        $startingPoint = count($rootLine) > 1 && $rootLine[0]['uid'] === 0 ? $rootLine[1]['uid'] : $startingPid;
-        $pageRecord = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord(
-            'pages',
-            $startingPoint
-        );
-
-        // Create and initialize the tree object
-        /** @var $tree \TYPO3\CMS\Backend\Tree\View\PageTreeView */
-        $tree = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Tree\\View\\PageTreeView');
-        $tree->init('AND ' . $GLOBALS['BE_USER']->getPagePermsClause(1));
-
-        // Creating the icon for the current page and add it to the tree
-        $verionNumberUtility = GeneralUtility::makeInstance(VersionNumberUtility::class);
-        $version = $verionNumberUtility->convertVersionStringToArray($verionNumberUtility->getNumericTypo3Version());
-        $is7up = $version['version_main'] > 7;
-        if ($is7up) {
-            /** @var IconFactory $iconFactory */
-            $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
-            $html = $iconFactory->getIconForRecord('pages', $pageRecord, Icon::SIZE_SMALL);
-        } else {
-            $html = \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIconForRecord(
-                'pages',
-                $pageRecord,
-                array(
-                    'title' => $pageRecord['title']
-                )
-            );
-        }
-        $tree->tree[] = array(
-            'row' => $pageRecord,
-            'HTML' => $html
-        );
-
-        // Create the page tree, from the starting point, 2 levels deep
-        $depth = 9;
-        $tree->getTree(
-            $startingPoint,
-            $depth,
-            ''
-        );
-
-        return $tree->tree;
-    }
-
-    protected function pageBrowser(int $pid)
-    {
-        $tree = $this->treeAction($pid);
     }
 }
