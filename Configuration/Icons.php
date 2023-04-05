02de421063746300ba9c960db9ef71add3cf83f7
Maik Schneider <schneider.maik@me.com>
2023-04-05 18:09:41 +0200

    feat: refactor ext_localconf.php, add Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'bw_focuspoint_images_svg' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:bw_focuspoint_images/Resources/Public/Icons/ContentElements/FocuspointSVG.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,32 +1,32 @@
 <?php
 
-use Blueways\BwFocuspointImages\Hooks\BwFocuspointSvgPreviewRenderer;
+defined('TYPO3') || die();
 
-defined('TYPO3_MODE') || die('Access denied');
-
-/**
- * Register new form type
- */
+// Register custom form element
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1516966225] = [
     'nodeName' => 'focuspoint_wizard',
     'priority' => '70',
     'class' => Blueways\BwFocuspointImages\Form\Element\InputFocuspointElement::class,
 ];
 
-/**
- * Register icon
- */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'bw_focuspoint_images_svg',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:bw_focuspoint_images/Resources/Public/Icons/ContentElements/FocuspointSVG.svg']
-);
+$versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 
-// Register for hook to show preview of tt_content element of CType="textmedia" in page module
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['bw_focuspoint_images_svg'] = BwFocuspointSvgPreviewRenderer::class;
+if ($versionInformation->getMajorVersion() < 11) {
+    // Register Icon
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'bw_focuspoint_images_svg',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:bw_focuspoint_images/Resources/Public/Icons/ContentElements/FocuspointSVG.svg']
+    );
+}
 
-// Register PageTS
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:bw_focuspoint_images/Configuration/page.tsconfig">
-');
+if ($versionInformation->getMajorVersion() < 12) {
+    // Register PageTS
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        @import \'EXT:bw_focuspoint_images/Configuration/page.tsconfig\'
+    ');
+
+    // Register for hook to show preview of tt_content element of CType="bw_focuspoint_images_svg" in page module
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['bw_focuspoint_images_svg'] = \Blueways\BwFocuspointImages\Hooks\BwFocuspointSvgPreviewRenderer::class;
+}
