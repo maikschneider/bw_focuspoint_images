b952b2d0e859b27ed40e65d180720c107ef84a2a
Maik Schneider <mas@xima.de>
2025-03-18 13:10:22 +0100

    feat: remove inline css from backend preview

--- Classes/Preview/FocuspointPreviewRenderer.php
+++ Classes/Preview/FocuspointPreviewRenderer.php
@@ -5,12 +5,16 @@ namespace Blueways\BwFocuspointImages\Preview;
 use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * Contains a preview rendering for the page module of CType="textmedia"
- *
- * @internal this is a concrete TYPO3 hook implementation and solely used for EXT:frontend and not part of TYPO3's Core API.
- */
+* Contains a preview rendering for the page module of CType="textmedia"
+*
+* @internal this is a concrete TYPO3 hook implementation and solely used for EXT:frontend and not part of TYPO3's Core API.
+*/
 class FocuspointPreviewRenderer extends StandardContentPreviewRenderer
 {
     public function renderPageModulePreviewContent(GridColumnItem $item): string
@@ -22,25 +26,9 @@ class FocuspointPreviewRenderer extends StandardContentPreviewRenderer
             return $content;
         }
 
-        $content .= BackendUtility::thumbCode(
-            $row,
-            'tt_content',
-            'assets',
-            '',
-            '',
-            null,
-            0,
-            '',
-            '200px',
-            false
-        );
-
-        $content .= '<style>.focuspoint .preview-thumbnails-element-image {height:auto;}</style>';
-
         $fileReferences = BackendUtility::resolveFileReferences('tt_content', 'assets', $row);
-
         if ($fileReferences === null || $fileReferences === []) {
-            return $this->linkEditContent($content, $row);
+            return '';
         }
 
         $fileReference = $fileReferences[array_key_first($fileReferences)];
@@ -50,13 +38,50 @@ class FocuspointPreviewRenderer extends StandardContentPreviewRenderer
             return $this->linkEditContent($content, $row);
         }
 
-        $points = json_decode((string)$focuspoints, false) ?: [];
-        if (empty($points)) {
-            return $this->linkEditContent($content, $row);
+        $content .= '<div class="preview-thumbnails" style="--preview-thumbnails-size: 200px">';
+
+        foreach ($fileReferences as $reference) {
+            $image = $reference->getOriginalFile()->process(
+                ProcessedFile::CONTEXT_IMAGEPREVIEW,
+                [
+                    'width' => '200m',
+                    'height' => '200m',
+                ]
+            );
+
+            $content .= '<div class="preview-thumbnails-element">';
+            $content .= '<div class="preview-thumbnails-element-image">';
+            $content .= '<div class="bw-focuspoint-image">';
+            $content .= '<img src="' . $image->getPublicUrl() . '" width="200" height="200" loading="lazy" alt="Focuspoint image"/>';
+            $content .= $this->getSvgForFileReference($reference);
+            $content .= '</div>';
+            $content .= '</div>';
+            $content .= '</div>';
         }
 
-        $svg = '<svg viewBox="0 0 200 200" preserveAspectRatio="none" style="height:100%; width:100%; top:0; left:0; position:absolute;" width="200" class="focuspoint__svg" xmlns="http://www.w3.org/2000/svg">
-            <mask id="mask' . array_key_first($fileReferences) . '"><rect x="0" y="0" width="200" height="200" fill="#FFF" fill-opacity="0.5" />';
+        $content .= '</div>';
+
+        /** @var PageRenderer $pageRenderer */
+        $pageRenderer = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+        $pageRenderer->addCssFile('EXT:bw_focuspoint_images/Resources/Public/Css/BackendPreview.css');
+
+        return $this->linkEditContent($content, $row);
+    }
+
+    private function getSvgForFileReference(FileReference $fileReference): string
+    {
+        $focuspoints = $fileReference->getReferenceProperty('focus_points');
+        if ($focuspoints === null || $focuspoints === '') {
+            return '';
+        }
+
+        $points = json_decode((string)$focuspoints, false) ?: [];
+        if (empty($points)) {
+            return '';
+        }
+
+        $svg = '<svg viewBox="0 0 200 200" preserveAspectRatio="none" class="focuspoint__svg" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask' . $fileReference->getUid() . '"><rect x="0" y="0" width="200" height="200" fill="#FFF" fill-opacity="0.5" />';
 
         foreach ($points as $point) {
             $x = $point->x * 100;
@@ -65,14 +90,14 @@ class FocuspointPreviewRenderer extends StandardContentPreviewRenderer
             $width = $point->width * 100;
 
             $svg .= '<rect x="' . $x . '%"
-                  y="' . $y . '%"
-                  width="' . $width . '%"
-                  height="' . $height . '%"
-                  fill="#000"/>';
+                y="' . $y . '%"
+                width="' . $width . '%"
+                height="' . $height . '%"
+                fill="#000"/>';
         }
 
         $svg .= '</mask>';
-        $svg .= '<rect x="0" y="0" width="200" height="200" fill="#000" mask="url(#mask' . array_key_first($fileReferences) . ')" />';
+        $svg .= '<rect x="0" y="0" width="200" height="200" fill="#000" mask="url(#mask' . $fileReference->getUid() . ')" />';
 
         foreach ($points as $point) {
             $x = $point->x * 100;
@@ -81,18 +106,15 @@ class FocuspointPreviewRenderer extends StandardContentPreviewRenderer
             $width = $point->width * 100;
 
             $svg .= '<rect x="' . $x . '%"
-                  y="' . $y . '%"
-                  stroke="#ff8700"
-                  stroke-width="1.5px"
-                  width="' . $width . '%"
-                  height="' . $height . '%"
-                  fill="none"/>';
+                y="' . $y . '%"
+                stroke="#ff8700"
+                stroke-width="1.5px"
+                width="' . $width . '%"
+                height="' . $height . '%"
+                fill="none"/>';
         }
 
         $svg .= '</svg>';
-        $content .= $svg;
-        $content = '<div class="focuspoint" style="display:inline-block; position:relative;">' . $content . '</div>';
-
-        return $this->linkEditContent($content, $row);
+        return $svg;
     }
 }
