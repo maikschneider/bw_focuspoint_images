cd6afe455eb8b03ecffcb7917788dc6e7d90631a
Maik Schneider <mas@xima.de>
2025-03-18 14:07:22 +0100

    fix: do not include backend wizard js in frontend (#41)

--- Classes/EventListener/BeforeJavaScriptsRenderingEventListener.php
+++ Classes/EventListener/BeforeJavaScriptsRenderingEventListener.php
@@ -2,13 +2,16 @@
 
 namespace Blueways\BwFocuspointImages\EventListener;
 
+use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Page\Event\BeforeJavaScriptsRenderingEvent;
 
 class BeforeJavaScriptsRenderingEventListener
 {
     public function __invoke(BeforeJavaScriptsRenderingEvent $event): void
     {
-        $event->getAssetCollector()->addJavaScript('focuspoint-wizard',
-            'EXT:bw_focuspoint_images/Resources/Public/JavaScript/FocuspointWizard.js', ['type' => 'module']);
+        if (ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()) {
+            $event->getAssetCollector()->addJavaScript('focuspoint-wizard',
+                'EXT:bw_focuspoint_images/Resources/Public/JavaScript/FocuspointWizard.js', ['type' => 'module']);
+        }
     }
 }
