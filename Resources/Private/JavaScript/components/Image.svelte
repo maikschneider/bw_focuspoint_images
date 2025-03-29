2c1d3bcefd94c2a1edb0de4c0222ac14089b8f40
Maik Schneider <mas@xima.de>
2025-03-29 18:03:57 +0100

    fix: improve css selectors to drop !important

--- Resources/Private/JavaScript/components/Image.svelte
+++ Resources/Private/JavaScript/components/Image.svelte
@@ -157,7 +157,7 @@
         opacity: 0.8;
         background-image: linear-gradient(45deg, var(--chess-color) 25%, transparent 25%), linear-gradient(-45deg, var(--chess-color) 25%, transparent 25%), linear-gradient(45deg, transparent 75%, var(--chess-color) 75%), linear-gradient(-45deg, transparent 75%, var(--chess-color) 75%);
         background-size: 20px 20px;
-        background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
+        background-position: 0 0, 0 10px, 10px -10px, -10px 0;
     }
 
     .cropper-bg--dark {
@@ -168,21 +168,20 @@
         align-self: center;
     }
 
-
-    .ui-resizable-nw, .ui-resizable-ne {
-        top: -3px !important;
+    .ui-resizable-handle.ui-resizable-nw, .ui-resizable-handle.ui-resizable-ne {
+        top: -3px;
     }
 
-    .ui-resizable-sw, .ui-resizable-se {
-        bottom: -3px !important;
+    .ui-resizable-handle.ui-resizable-sw, .ui-resizable-handle.ui-resizable-se {
+        bottom: -3px;
     }
 
-    .ui-resizable-ne, .ui-resizable-se {
-        right: -3px !important;
+    .ui-resizable-handle.ui-resizable-ne, .ui-resizable-handle.ui-resizable-se {
+        right: -3px;
     }
 
-    .ui-resizable-nw, .ui-resizable-sw {
-        left: -3px !important;
+    .ui-resizable-handle.ui-resizable-nw, .ui-resizable-handle.ui-resizable-sw {
+        left: -3px;
     }
 </style>
 
