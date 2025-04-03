0a6ac32b6dca6883325edfdac18a4a2c1a2adff0
Maik Schneider <mas@xima.de>
2025-04-03 17:15:30 +0200

    feat: display checkbox, not toggleCheckbox

--- Resources/Private/JavaScript/components/Fields/Checkbox.svelte
+++ Resources/Private/JavaScript/components/Fields/Checkbox.svelte
@@ -1,22 +1,31 @@
 <script>
-    import {focuspoints} from '../../store.svelte.js'
+    import {onMount} from "svelte";
+    import {focuspoints, getIcon, iconStore} from '../../store.svelte.js'
 
     let {config, index, name} = $props()
+
+    onMount(() => {
+        getIcon('actions-check')
+        getIcon('empty-empty')
+    })
 </script>
 
 <div class="form-group">
     <label class="form-label" for="input-{index}-{name}">
         {config.title}
     </label>
-    <div class="form-group">
-        <label class="form-label" for="input-{index}-{name}">
-            {config.title}
+    <div class="form-check form-check-type-icon-toggle">
+        <input type="checkbox" class="form-check-input" id="input-{index}-{name}" bind:checked={$focuspoints[index][name]} />
+        <label class="form-check-label" for="input-{index}-{name}">
+            <span class="form-check-label-icon">
+                <span class="form-check-label-icon-checked">
+                    {@html $iconStore['actions-check']}
+                </span>
+                <span class="form-check-label-icon-unchecked">
+                    {@html $iconStore['empty-empty']}
+                </span>
+            </span>
+            {config?.label ?? ''}
         </label>
-        <div class="form-check form-switch">
-            <input type="checkbox" class="form-check-input" id="input-{index}-{name}" bind:checked={$focuspoints[index][name]} />
-            <label class="form-check-label" for="input-{index}-{name}">
-                <span class="visually-hidden">{config.title}</span>
-            </label>
-        </div>
     </div>
 </div>
