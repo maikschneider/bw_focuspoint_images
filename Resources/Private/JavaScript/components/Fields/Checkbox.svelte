6cee60a7fa6e841d9385d354de83a2c3750c7831
Maik Schneider <mas@xima.de>
2025-04-06 15:16:39 +0200

    feat: add renderType checkboxToggle

--- Resources/Private/JavaScript/components/Fields/Checkbox.svelte
+++ Resources/Private/JavaScript/components/Fields/Checkbox.svelte
@@ -4,6 +4,9 @@
 
     let {config, index, name} = $props()
 
+    let isCheckbox = config?.renderType === 'check' || !Object.hasOwn(config, 'renderType')
+    let isToggle = config?.renderType === 'checkboxToggle'
+
     onMount(() => {
         getIcon('actions-check')
         getIcon('empty-empty')
@@ -11,24 +14,28 @@
 </script>
 
 <div class="form-group">
-    <label class="form-label" for="input-{index}-{name}">
+    <label class="form-label" for="input-{index}-{name}" id="label-{index}-{name}">
         {config?.title ?? config.title}
     </label>
-    <div class="form-check form-check-type-icon-toggle">
+    <div
+        class="form-check" class:form-check-type-icon-toggle={isCheckbox} class:form-switch={isToggle}>
         <input
             type="checkbox"
-            class="form-check-input"
+            class="form-check-input me-1"
+            id="input-{index}-{name}"
             bind:checked={$focuspoints[index][name]}
             aria-labelledby="label-{index}-{name}" />
         <label class="form-check-label" for="input-{index}-{name}">
-            <span class="form-check-label-icon">
-                <span class="form-check-label-icon-checked">
-                    {@html $iconStore['actions-check']}
+            {#if isCheckbox}
+                <span class="form-check-label-icon">
+                    <span class="form-check-label-icon-checked">
+                        {@html $iconStore['actions-check']}
+                    </span>
+                    <span class="form-check-label-icon-unchecked">
+                        {@html $iconStore['empty-empty']}
+                    </span>
                 </span>
-                <span class="form-check-label-icon-unchecked">
-                    {@html $iconStore['empty-empty']}
-                </span>
-            </span>
+            {/if}
             {config?.label ?? ''}
         </label>
     </div>
