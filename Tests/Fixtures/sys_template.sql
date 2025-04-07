4440c543b7af7cf139e13696783ae36af4a21c23
Maik Schneider <mas@xima.de>
2025-04-07 15:17:09 +0200

    feat: add typoscript fixture for frontend output

---
+++ Tests/Fixtures/sys_template.sql
@@ -0,0 +1,2 @@
+insert into `sys_template` (`uid`, `pid`, `title`, `root`, `clear`, `include_static_file`)
+values (1, 1, 'MAIN TEMPLATE', 1, 3, 'EXT:bootstrap_package/Configuration/TypoScript,EXT:bw_focuspoint_images/Configuration/TypoScript');
