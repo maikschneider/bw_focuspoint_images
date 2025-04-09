5fc0f8210103bd9173f7db5b86685d4fabfcd716
Maik Schneider <mas@xima.de>
2025-04-09 11:51:07 +0200

    feat: remove old typolink migration in data processor

--- Classes/DataProcessing/FocuspointProcessor.php
+++ Classes/DataProcessing/FocuspointProcessor.php
@@ -46,16 +46,6 @@ class FocuspointProcessor extends FilesProcessor
                 $point->textY = $point->y + ($point->height / 2);
 
                 foreach ($point as $fieldName => &$fieldValue) {
-                    // in case of old typolink syntax (v2.3.3): replace link field with typolink value
-                    if (is_object($fieldValue) && property_exists($fieldValue, 'key')) {
-                        $newLink = 't3://' . $fieldValue->key . '?uid=' . $fieldValue->uid;
-                        if (property_exists($fieldValue, 'target') && $fieldValue->target) {
-                            $newLink .= ' ' . $fieldValue->target;
-                        }
-
-                        $fieldValue = $newLink;
-                    }
-
                     // in case of typolinks with target, add a new field {$fieldName}Target='_blank'
                     if (is_string($fieldValue) && strpos($fieldValue, 't3://') === 0) {
                         $linkValues = $typoLinkCodecService->decode($fieldValue);
