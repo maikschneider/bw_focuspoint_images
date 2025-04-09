e64bf750da597782b90fac3ca677f5c1641e6d7a
Maik Schneider <mas@xima.de>
2025-04-09 11:52:22 +0200

    fix: add packaging_exclude.php for TER release

---
+++ packaging_exclude.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'directories' => [
+        '.build',
+        '.ddev',
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'Resources/Private/JavaScript',
+        'tailor-version-upload',
+        'Tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CODE_OF_CONDUCT.md',
+        'codeception.yml',
+        'codecov.yml',
+        'CODEOWNERS',
+        'composer.lock',
+        'CONTRIBUTING.md',
+        'crowdin.yaml',
+        'dependency-checker.json',
+        'docker-compose.yml',
+        'editorconfig',
+        'editorconfig-lint.php',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpstan.neon',
+        'phpstan-baseline.neon',
+        'phpunit.functional.xml',
+        'phpunit.unit.xml',
+        'rector.php',
+        'renovate.json',
+        'typoscript-lint.yml',
+    ],
+];
