27e55ff192280c6557dd9d442ce0714eff2eb8a0
Nail Gafarov <nail.gafarov@bikar.com>
2025-04-16 10:02:38 +0200

    add polygon points at the closest segment

--- Resources/Private/JavaScript/components/Image.svelte
+++ Resources/Private/JavaScript/components/Image.svelte
@@ -19,7 +19,6 @@
             move(event) {
                 const index = parseInt(event.target.getAttribute('data-index'));
                 $focuspoints[index].points = $focuspoints[index].points.map(([x, y]) => [x + event.dx, y + event.dy]);
-                console.log($activeIndex);
             },
             end: onDraggableEnd
         }
@@ -80,13 +79,42 @@
         const rect = event.target.getBoundingClientRect();
         const viewBox = event.target.viewBox.baseVal;
         const ratio = viewBox.width / rect.width;
-        $focuspoints[$activeIndex].points = [...$focuspoints[$activeIndex].points, [event.layerX * ratio, event.layerY * ratio]];
+        const point = [event.layerX * ratio, event.layerY * ratio];
+        const index = findClosestMiddlePointIndex(point);
+        const points = $focuspoints[$activeIndex].points.slice();
+        points.splice(index + 1, 0, point);
+        $focuspoints[$activeIndex].points = points;
     }
 
     function onCircleDblClick(event) {
         const index = parseInt(event.target.getAttribute('data-index'));
         const pointIndex = parseInt(event.target.getAttribute("data-point-index"));
         $focuspoints[index].points = $focuspoints[index].points.filter((point, i) => i !== pointIndex);
+    }
+
+    function findClosestMiddlePointIndex(point) {
+        const points = $focuspoints[$activeIndex].points;
+        const middlePoints = [...points, points[0]].reduce((acc, cur, i, arr) => [...acc, [cur, arr[i + 1]]], []).slice(0, -1).map(segment => {
+            const [[x1, y1], [x2, y2]] = segment;
+            return [
+                x1 + (x2 - x1) / 2,
+                y1 + (y2 - y1) / 2
+            ];
+        });
+        let index = 0;
+        let closest = [Infinity, Infinity];
+        for (const i in middlePoints) {
+            const middlePoint = middlePoints[i];
+            if (distance(point, middlePoint) < distance(point, closest)) {
+                closest = middlePoint;
+                index = i;
+            }
+        }
+        return +index;
+    }
+
+    function distance([x1, y1], [x2, y2]) {
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
     }
 
     export function updateCanvasSizes() {
