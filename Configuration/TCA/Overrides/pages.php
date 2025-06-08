bb6d109b57bb156bda220365de9722bbda9878eb
Maik Schneider <mas@xima.de>
2025-06-08 21:01:41 +0200

    feat: streamline element name

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -7,5 +7,5 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 ExtensionManagementUtility::registerPageTSConfigFile(
     'bw_focuspoint_images',
     'Configuration/TsConfig/Page/newContentElement.tsconfig',
-    'Bw Focuspoint Images Content Element',
+    'Focuspoint Images: Content Element',
 );
