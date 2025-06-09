d0f3d94f1fe36b7ad28a5271059fee9a7230fbf3
Maik Schneider <mas@xima.de>
2025-06-09 10:07:54 +0200

    chore: raise version

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,5 +20,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Maik Schneider',
     'author_email' => 'maik.schneider@xima.de',
     'author_company' => 'XIMA Media GmbH',
-    'version' => '5.2.0',
+    'version' => '6.0.0',
 ];
