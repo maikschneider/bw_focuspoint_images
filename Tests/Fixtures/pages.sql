625303d4a0215c737e9501f73f64d376a4ecfca6
Maik Schneider <maik.schneider@xima.de>
2026-04-04 19:02:20 +0200

    feat: add example site set
    # Conflicts:
    #	Tests/Fixtures/pages.sql

--- Tests/Fixtures/pages.sql
+++ Tests/Fixtures/pages.sql
@@ -1,3 +1,3 @@
 insert into `pages` (`uid`, `pid`, `title`, `slug`, `sys_language_uid`, `l10n_parent`, `l10n_source`, `perms_userid`,
-										 `perms_groupid`, `perms_user`, `perms_group`, `perms_everybody`, `doktype`, `is_siteroot`, `tsconfig_includes`)
-values (1, 0, 'Main', '/', 0, 0, 0, 1, 1, 31, 31, 1, 1, 1, 'EXT:bw_focuspoint_images/Configuration/TsConfig/Page/newContentElement.tsconfig');
+										 `perms_groupid`, `perms_user`, `perms_group`, `perms_everybody`, `doktype`, `is_siteroot`, `TSconfig`)
+values (1, 0, 'Main', '/', 0, 0, 0, 1, 1, 31, 31, 1, 1, 1, '');
