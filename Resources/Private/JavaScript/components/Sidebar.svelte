433917f4aef7d6df27a103385bd5e76a0b93b096
Can Adrian Karadag <can.karadag@bikar.com>
2026-04-08 11:30:41 +0200

    feat(sidebar): display link additional attributes
    - Show callout with additional attributes in link field
    - Render HTML preview from linkBrowserData

--- Resources/Private/JavaScript/components/Sidebar.svelte
+++ Resources/Private/JavaScript/components/Sidebar.svelte
@@ -3,10 +3,9 @@
         focuspoints,
         createNewFocuspoint,
         wizardConfigStore,
-        setActiveIndex,
         focusPointName, fieldMeetsCondition,
-        getActiveIndex,
-        SHAPES,
+        activateFocuspoint,
+        SHAPES, type Focuspoint,
     } from '../store.svelte'
     import type {ShapeType} from "../store.svelte";
     import Select from "./Fields/Select.svelte";
@@ -15,6 +14,8 @@
     import Link from "./Fields/Link.svelte";
     import Checkbox from "./Fields/Checkbox.svelte";
     import Icon from './Icon.svelte';
+
+    let focuspointName = $derived((focuspoint: Focuspoint, index: number) => focusPointName(index))
 
     function deleteFocuspoint(index: number) {
         $focuspoints = $focuspoints.filter((focuspoint, i) => i !== index)
@@ -27,6 +28,15 @@
         link: Link,
         checkbox: Checkbox
     };
+
+    function getFieldComponent(type ?: string) {
+        if (!type) return null;
+        return components[type as keyof typeof components] ?? null;
+    }
+
+    function toShapeType(key: string) {
+        return key as ShapeType;
+    }
 </script>
 
 
@@ -44,44 +54,32 @@
         margin-bottom: 0;
     }
 
-    .v12 .panel-button {
-        border: 0;
-        color: #FFF;
-        background: none;
-        gap: 10px;
-        align-items: center;
-        justify-content: start !important;
-        width: 100%;
+    :global(.callout) {
+        --typo3-component-border-radius: 4px;
     }
-
-    .v12 .panel-button[aria-expanded="true"] {
-        border-left: 2px solid #ff8700;
-    }
-
-    .v12 .caret {
-        border-top-color: #FFF;
-    }
-
 </style>
 
-<div class="modal-panel-sidebar" class:v12={$wizardConfigStore && $wizardConfigStore.typo3Version! < 13}>
+<div class="modal-panel-sidebar">
     <div class="panel-group" role="tablist" aria-multiselectable="false">
         {#each $focuspoints as focuspoint, index}
             <div class="panel panel-default" data-crop-variant-container="default">
                 <div class="panel-heading" role="tab">
                     <h4 class="panel-title" id="cropper-accordion-heading-{index}">
                         <button
-                            on:click={() => setActiveIndex(index)}
+                            onclick={(e) => {
+                                e.preventDefault()
+                                activateFocuspoint(index)
+                            }}
                             data-bs-toggle="collapse"
-                            aria-expanded={index === getActiveIndex()}
-                            aria-controls="cropper-collapse-1"
-                            class:collapsed={index !== getActiveIndex()}
-                            class="panel-button"
-                            data-crop-variant-id="default"
-                            data-crop-variant="">
+                            data-bs-target="#cropper-collapse-{index}"
+                            aria-expanded={focuspoint.active}
+                            aria-controls="cropper-collapse-{index}"
+                            class:collapsed={!focuspoint.active}
+                            class:show={focuspoint.active}
+                            class="panel-button">
                             <span class="caret"></span>
                             <span class="panel-title">
-                                {focusPointName(index)}
+                                {focuspointName(focuspoint, index)}
                             </span>
                         </button>
                     </h4>
@@ -89,20 +87,26 @@
                 <div
                     id="cropper-collapse-{index}"
                     class="panel-collapse"
-                    class:collapse={index !== getActiveIndex()}
-                    class:show={index === getActiveIndex()}
+                    class:show={focuspoint.active}
+                    class:collapse={!focuspoint.active}
                     role="tabpanel"
                     aria-labelledby="cropper-accordion-heading-{index}">
                     <div class="panel-body">
                         {#each Object.entries($wizardConfigStore.fields) as [key, field]}
                             {#if fieldMeetsCondition(key, focuspoint) && field.type && field.type in components}
-                                <svelte:component this={components[field.type as keyof typeof components]} index={index} name={key} config={field ?? {}} />
+                                {@const FieldComponent = getFieldComponent(field.type)}
+                                {#if FieldComponent}
+                                    <FieldComponent index={index} name={key} config={field ?? {}} />
+                                {/if}
                             {/if}
                         {/each}
 
-                        <button class="btn btn-danger" name="reset" title="Reset" on:click|preventDefault={() => deleteFocuspoint(index)}>
+                        <button class="btn btn-danger" name="reset" title="Reset" onclick={(e) => {
+                                e.preventDefault()
+                                deleteFocuspoint(index)
+                            }}>
                             <Icon name="actions-delete" />
-                            {window.parent.frames.list_frame.TYPO3.lang['wizard.single_point.button.delete']}
+                            {$wizardConfigStore?.lang['wizard.single_point.button.delete']}
                         </button>
                     </div>
                 </div>
@@ -111,9 +115,12 @@
     </div>
 
     {#each Object.entries(SHAPES) as [key]}
-        <button class="btn btn-success w-100 mt-3" on:click|preventDefault={() => createNewFocuspoint(key as ShapeType)}>
+        <button class="btn btn-success w-100 mt-3" onclick={(e) => {
+            e.preventDefault()
+            createNewFocuspoint(toShapeType(key))
+        }}>
             <Icon name="actions-add" />
-            {window.parent.frames.list_frame.TYPO3.lang[`wizard.single_point.button.new.${key}`]}
+            {$wizardConfigStore?.lang[`wizard.single_point.button.new.${key}`]}
         </button>
     {/each}
 </div>
